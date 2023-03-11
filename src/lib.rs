71e23ba6feff0bf317c6c31d0b41b48218905895
chenquan <chenquan.dev@gmail.com>
2023-03-11 18:15:40 +0800

    feat(load-balance): add p2c

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(type_alias_impl_trait)]
+#![feature(duration_constants)]
 
 mod discover;
 mod loadbalance;
@@ -9,11 +10,14 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
+    use num_integer::Roots;
     use super::*;
 
     #[test]
     fn it_works() {
         let result = add(2, 2);
+        let x =22;
+        let i = x.sqrt();
         assert_eq!(result, 4);
     }
 }
--- src/loadbalance/p2c.rs
+++ src/loadbalance/p2c.rs
@@ -1,19 +1,26 @@
 use std::future::Future;
 use std::hash::Hash;
 use std::marker::PhantomData;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
+use std::time;
+use std::sync::atomic::{AtomicI64, AtomicU64, Ordering};
+use std::time::{SystemTime, UNIX_EPOCH};
 use dashmap::DashMap;
+use dashmap::mapref::entry::Entry;
 use volo::context::Endpoint;
-use volo::discovery::{Change, Discover};
+use volo::discovery::{Change, Discover, Instance};
+use volo::layer::LayerExt;
 use volo::loadbalance::error::LoadBalanceError;
 use volo::loadbalance::LoadBalance;
-use volo::loadbalance::random::InstancePicker;
+use volo::net::Address;
+use num_integer::Roots;
+use volo::new_type;
 
 pub struct P2c<K>
     where K: Hash + PartialEq + Eq + Send + Sync + 'static
 
 {
-    router: DashMap<K, Arc<String>>,
+    router: DashMap<K, Vec<Arc<SubConn>>>,
 
 }
 
@@ -27,12 +34,124 @@ impl<D> LoadBalance<D> for P2c<D::Key>
             Self: 'future;
 
     fn get_picker<'future>(&'future self, endpoint: &'future Endpoint, discover: &'future D) -> Self::GetFut<'future> where Self: 'future {
-        todo!()
+        async {
+            let key = discover.key(endpoint);
+            let _ = match self.router.entry(key) {
+                Entry::Occupied(e) => {
+                    // e.get().clone()
+                }
+                Entry::Vacant(e) => {}
+            };
+            //
+            // let x: Vec<Arc<Instance>> = discover
+            //     .discover(endpoint)
+            //     .await
+            //     .map_err(|err| err.into())?;
+
+            Ok(InstancePicker { conns: Arc::new(vec![]) })
+        }
     }
 
     fn rebalance(&self, changes: Change<D::Key>) {
-        todo!()
+        // match self.router.entry(changes.key) {
+        //     Entry::Occupied(e) => {
+        //         // changes.removed
+        //
+        //         e.replace_entry(Arc::new(InstancePicker {}));
+        //     }
+        //     Entry::Vacant(e) => {}
+        // }
     }
 }
 
-struct SubConn {}
+pub struct InstancePicker {
+    // 可用的连接
+    conns: Arc<Vec<SubConn>>,
+}
+
+impl InstancePicker {
+    fn new(conns: Arc<Vec<SubConn>>) -> Self {
+        Self { conns }
+    }
+}
+
+const INIT_SUCCESS: i64 = 1000;
+const THROTTLE_SUCCESS: i64 = INIT_SUCCESS / 2;
+const PENALTY: i64 = i64::MAX;
+const forcePick: time::Duration = time::Duration::SECOND;
+
+
+struct SubConn {
+    lag: AtomicU64,
+    inflight: AtomicI64,
+    success: AtomicU64,
+    requests: AtomicI64,
+    last: AtomicI64,
+    pick: AtomicI64,
+    instance: Instance,
+}
+
+impl SubConn {
+    fn healthy(&self) -> bool {
+        self.success.load(Ordering::SeqCst) > THROTTLE_SUCCESS as u64
+    }
+
+    fn load(&self) -> i64 {
+        let lag: i64 = (self.lag.load(Ordering::SeqCst) + 1).sqrt() as i64;
+        let load = lag * (self.inflight.load(Ordering::SeqCst) + 1);
+        if load == 0 {
+            return PENALTY;
+        }
+
+        load
+    }
+}
+
+
+impl Iterator for InstancePicker {
+    type Item = Address;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let conns = &self.conns;
+        match conns.len() {
+            0 => {
+                None
+            }
+            1 => {
+                Some(conns[0].instance.address.clone())
+            }
+            2 => {
+                None
+            }
+            _ => {
+                None
+            }
+        }
+    }
+}
+
+impl InstancePicker {
+    fn choose(&self, c1: SubConn, c2: Option<SubConn>) -> SubConn {
+        let now = chrono::Utc::now().timestamp();
+        if c2.is_none() {
+            c1.last.store(now, Ordering::SeqCst);
+            return c1;
+        }
+        let c2 = c2.unwrap();
+
+        let mut c1 = c1;
+        let mut c2 = c2;
+
+        if c1.load() > c2.load() {
+            let t = c1;
+            c1 = c2;
+            c2 = t;
+        }
+
+        let pick = c2.pick.load(Ordering::SeqCst);
+        forcePick.as_millis() as i64
+        // if now - pick >forcePick &&
+        c2.pick.compare_and_swap(pick, now, Ordering::SeqCst);
+        c1
+    }
+}
