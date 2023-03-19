a17888008b11d6368f585619d23148293f8c2ca9
chen quan <chenquan.dev@gmail.com>
2023-03-19 13:15:38 +0800

    feat(load-balance): delete p2c (#1)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![feature(type_alias_impl_trait)]
+#![feature(duration_constants)]
 
-mod discover;
-mod loadbalance;
+pub mod discover;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -9,11 +9,15 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
+    use num_integer::Roots;
     use super::*;
+    use discover;
 
     #[test]
     fn it_works() {
         let result = add(2, 2);
+        let x = 22;
+        let i = x.sqrt();
         assert_eq!(result, 4);
     }
 }
